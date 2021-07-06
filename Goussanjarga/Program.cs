56deb08ff1429758ffb3dc1f3349212678255eb6
Alf Ragnar <69853212+AlfRagnar@users.noreply.github.com>
2021-07-06 13:57:36 +0200

    Changed Project to MVC

---
+++ Goussanjarga/Program.cs
@@ -0,0 +1,28 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Goussanjarga
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+                config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+            })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
